a020df67114705f89dad1e3885e8abae06512b66
Michael Malak <michael.k.malak@gmail.com>
2020-05-25 13:23:18 +0200

    Update Coordinates.kt

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -1,7 +1,7 @@
-val PUBLISHING_GROUP = "com.solikil.et2amel"
+val PUBLISHING_GROUP = "com.solikil.kotlin.et2amel"
 
 object AppCoordinates {
-    const val APP_ID = "com.solikil.et2amel.app"
+    const val APP_ID = "com.solikil.kotlin.et2amel.app"
 
     const val APP_VERSION_NAME = "1.0.0"
     const val APP_VERSION_CODE = 1
