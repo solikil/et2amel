35a09cad8ec343b5813c7403d5cf268e05d5b408
Msoliman512 <mesaam17@gmail.com>
2020-05-28 20:28:35 +0200

    Create Skeleton interactor, presenter, view

--- app/src/androidTest/java/com/solikil/kotlin/et2amel/app/MainActivityTest.kt
+++ app/src/androidTest/java/com/solikil/kotlin/et2amel/app/MainActivityTest.kt
@@ -10,6 +10,7 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import com.solikil.kotlin.et2amel.app.mainScreen.view.MainActivity
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
--- app/src/main/java/com/solikil/kotlin/et2amel/app/mainScreen/view/MainActivity.kt
+++ app/src/main/java/com/solikil/kotlin/et2amel/app/mainScreen/view/MainActivity.kt
@@ -1,13 +1,20 @@
-package com.solikil.kotlin.et2amel.app
+package com.solikil.kotlin.et2amel.app.mainScreen.view
 
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.solikil.kotlin.et2amel.app.FactorialCalculator
+import com.solikil.kotlin.et2amel.app.NotificationUtil
+import com.solikil.kotlin.et2amel.app.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val notificationUtil: NotificationUtil by lazy { NotificationUtil(this) }
+    private val notificationUtil: NotificationUtil by lazy {
+        NotificationUtil(
+            this
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +22,9 @@ class MainActivity : AppCompatActivity() {
 
         button_compute.setOnClickListener {
             val input = edit_text_factorial.text.toString().toInt()
-            val result = FactorialCalculator.computeFactorial(input).toString()
+            val result = FactorialCalculator.computeFactorial(
+                input
+            ).toString()
 
             text_result.text = result
             text_result.visibility = View.VISIBLE
