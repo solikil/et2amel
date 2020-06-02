e86cdf29fd5439e325cc127830b1e16a441ed53e
12827629 <michael.k.malak@gmail.com>
2020-06-02 23:38:33 +0200

    Fix build errors

--- app/src/main/java/com/solikil/kotlin/et2amel/app/mainScreen/features/timerFeature/presenter/TimerPresenterImp.kt
+++ app/src/main/java/com/solikil/kotlin/et2amel/app/mainScreen/features/timerFeature/presenter/TimerPresenterImp.kt
@@ -1,3 +1,10 @@
 package com.solikil.kotlin.et2amel.app.mainScreen.features.timerFeature.presenter
 
-class TimerPresenterImp : TimerPresenter { /*...*/ }
+import com.solikil.kotlin.et2amel.app.mainScreen.features.timerFeature.view.TimerView
+import com.solikil.kotlin.et2amel.app.mainScreen.interactor.MainScreenInteractor
+import javax.inject.Inject
+
+class TimerPresenterImp @Inject constructor(
+    private val timerView: TimerView,
+    private val mainScreenInteractor: MainScreenInteractor
+) : TimerPresenter { /*...*/ }
